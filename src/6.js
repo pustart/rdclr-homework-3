96ec2eac18575e729199039963b9ccf9804f2a77
pustart <polaris9955@gmail.com>
2023-08-20 22:23:28 +0300

    feat: add task 6

--- src/6.js
+++ src/6.js
@@ -1,14 +1,31 @@
 const completionFlags = [];
 
+// Изначальный порядок: [2, 1, 4, 3]
 async function asyncActions() {
-    // ========== Начало зоны редактирования ===============
+  // ========== Начало зоны редактирования ===============
+  queueMicrotask(() => {
     action('2');
-    action('1');
+  });
+
+  action('1');
+
+  setTimeout(() => {
     action('4');
+  }, 0);
+
+  queueMicrotask(() => {
     action('3');
-    // ========== Конец зоны редактирования ===============
+  });
+  // ========== Конец зоны редактирования ===============
 }
 
 function action(pos) {
-    completionFlags.push(pos);
+  completionFlags.push(pos);
 }
+
+asyncActions();
+
+// Функция для просмотра результата
+setTimeout(() => {
+  console.log(completionFlags);
+}, 100);
